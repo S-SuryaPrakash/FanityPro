0edd8bb77a20b663829f9f5fee3b0cb2c1642ca4
S-SuryaPrakash <s.suryaprakash7767@gmail.com>
2026-07-01 22:35:08 +0530

    created a folder structure for the controller clas

--- src/main/java/com/example/contentfilter/Controller/HealthController.java
+++ src/main/java/com/example/contentfilter/Controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.example.contentfilter;
+package com.example.contentfilter.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
