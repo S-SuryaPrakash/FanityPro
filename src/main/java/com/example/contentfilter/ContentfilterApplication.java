f90638cca5cb70c84fa6a71e255f2b6c972671f0
S-SuryaPrakash <s.suryaprakash7767@gmail.com>
2026-07-01 22:26:17 +0530

    created a Get API

---
+++ src/main/java/com/example/contentfilter/ContentfilterApplication.java
@@ -0,0 +1,15 @@
+package com.example.contentfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentfilterApplication.class, args);
+	}
+
+}
+
+
